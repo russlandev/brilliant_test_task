83ca807125e2e09f0d778d2e5643845a35db51eb
Ruslan Tkachenko <ruslan.tkachenko.dev@gmail.com>
2025-05-13 22:58:31 +0300

    gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/brilliant_test_task',
+  assetPrefix: '/brilliant_test_task',
 };
 
 export default nextConfig;
