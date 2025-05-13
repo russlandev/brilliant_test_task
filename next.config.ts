6a8560e40e54b07049a7b46578c6d839abdc75d2
Ruslan Tkachenko <ruslan.tkachenko.dev@gmail.com>
2025-05-13 23:33:35 +0300

    vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/brilliant_test_task',
-  assetPrefix: '/brilliant_test_task',
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
